84690e9ca71aadcddb3636d133a7f96c5c8d89ee
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-09 22:31:46 -0300

    Update script.js

--- estudojs/mediadenotas/script/script.js
+++ estudojs/mediadenotas/script/script.js
@@ -1,12 +1,12 @@
 function calcularMedia() {
-    var tn1 = document.getElementById('txtn1')
-    var tn2 = document.getElementById('txtn2')
+    var tn1 = document.getElementById('txtn1').value
+    var tn2 = document.getElementById('txtn2').value
     var res = document.querySelector('div#res')
 
-    var n1 = Number(txtn1.value)
-    var n2 = Number(txtn2.value)
+    var n1 = Number(tn1)
+    var n2 = Number(tn2)
 
-    média = (n1 + n2) / 2
-    res.innerHTML='A média do aluno é ' + média + '!'
+    média = (n1 + n2) / 2  
+    res.innerHTML=`A média do aluno é ${média}!`
     
 }
