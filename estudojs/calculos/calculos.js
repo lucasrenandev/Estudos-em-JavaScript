c3b14bb00f559114fc61db105015167c23bd7820
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-12 14:37:19 -0300

    Update calculos.js

--- estudojs/calculos/calculos.js
+++ estudojs/calculos/calculos.js
@@ -2,12 +2,12 @@ function calcular() {
     let numero = Number(window.prompt('Informe um número!'))
     let res = document.querySelector('div#res')
 
-    res.innerHTML = `<p>O número a ser analisado sera o <strong>${numero}</strong></p><hr>` 
+    res.innerHTML = `<p>O número a ser analisado será o <strong>${numero}</strong></p><hr>` 
     res.innerHTML += `<p>O seu valor absoluto é o ${Math.abs(numero)}</p>`
     res.innerHTML += `<p>Sua parte inteira é o ${Math.trunc(numero)}</p>` 
-    res.innerHTML += `<p>O valor inteiro mais próximo é o ${Math.round(numero)}`
-    res.innerHTML += `Sua raiz quadrada é ${Math.sqrt(numero)}`
+    res.innerHTML += `<p>O valor inteiro mais próximo é o ${Math.round(numero)}</p>`
+    res.innerHTML += `<p>Sua raiz quadrada é ${Math.sqrt(numero)}</p>`
     res.innerHTML += `<p>Sua raiz cúbica é ${Math.cbrt(numero)}</p>`
     res.innerHTML += `<p>O valor de ${numero}² é ${Math.pow(numero, 2)}</p>`
-    res.innerHTML += `<p>O valor de ${numero}³ é ${Math.pow(numero, 3)}`
+    res.innerHTML += `<p>O valor de ${numero}³ é ${Math.pow(numero, 3)}</p>`
 }
