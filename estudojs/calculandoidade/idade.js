9b99ecd7d4b5b72ad276c00cb62466cc4f72c386
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-12 22:41:17 -0300

    calculando idade

---
+++ estudojs/calculandoidade/idade.js
@@ -0,0 +1,9 @@
+function calculoIdade() {
+    let agora = new Date
+    let ano = agora.getFullYear()
+    let nasc = window.prompt('Em qual ano você nasceu?')
+    let idade = ano - nasc
+    let res = document.querySelector('div#res')
+
+    res.innerHTML = `Quem nasceu em ${nasc} vai completar <mark>${idade}</mark> em ${ano} `
+}
