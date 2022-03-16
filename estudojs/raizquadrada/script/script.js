7e3d71a81cb0e4681975d3cf44b76cc96030bade
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-16 19:40:26 -0300

    Update script.js

--- estudojs/raizquadrada/script/script.js
+++ estudojs/raizquadrada/script/script.js
@@ -2,7 +2,7 @@ function verificarRaiz() {
     var n1 = document.getElementById('txtn1').value
     var res = document.getElementById('res')
 
-    var raiz = n1 * n1
+    var raiz = Number(n1 * n1)
 
-    res.innerHTML=`A raiz de ${n1} é igual a ${raiz}!`
+    res.innerHTML=`<p>A raiz de ${n1} é igual a ${raiz.toFixed(2)}!</p>`
 }
