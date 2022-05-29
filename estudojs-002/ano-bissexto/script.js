25d44b6c9e388306cdcd19b3187eccaf4b7c502a
Lucas Renan <lucasrenandns96@gmail.com>
2022-05-29 16:31:29 -0300

    Ano Bissexto

---
+++ estudojs-002/ano-bissexto/script.js
@@ -0,0 +1,32 @@
+
+let calcular = document.querySelector('#calcular')
+let data = new Date
+let ano_atual = agora.getFullYear()
+
+calcular.addEventListener('click', calculo_ano_bissexto)
+
+function calculo_ano_bissexto() {
+
+    const ano = document.querySelector('#ano').value
+    const saida = document.querySelector('.saida')
+    let resultado = ''
+
+    if (ano == '' || ano > ano_atual || ano < 0) {
+
+        window.alert('[ERRO] campo VAZIO ou ANO inválido. Tente novamente!')
+    }
+
+    else {
+
+        if (ano % 4 == 0 && ano % 100 !== 0 || ano % 400 == 0) {
+            resultado = `Analisando o ano <strong>${ano}</strong>, ele <strong>é BISSEXTO</strong>!`
+        }
+
+        else {
+            resultado = `Analisando o ano <strong>${ano}</strong>, ele <strong> não é BISSEXTO</strong>!`
+        }
+
+        saida.innerHTML = resultado + agora
+    }
+   
+}
