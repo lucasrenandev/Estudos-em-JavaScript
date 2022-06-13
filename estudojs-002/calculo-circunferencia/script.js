dae76674625ec5b1cda38ac0de66cee7d9749410
Lucas Renan <lucasrenandns96@gmail.com>
2022-06-13 19:44:20 -0300

    Update script.js

--- estudojs-002/calculo-circunferencia/script.js
+++ estudojs-002/calculo-circunferencia/script.js
@@ -6,7 +6,7 @@ button.addEventListener('click', calculoCircunferência)
 function calculoCircunferência() {
     
     const resultado = document.getElementById("resultado")
-    raio = Number(window.prompt('Qual o raio do círculo?'))
+    const raio = Number(window.prompt('Qual o raio do círculo?'))
     
     if (raio == '' || raio !== Number(raio)) {
 
@@ -14,13 +14,14 @@ function calculoCircunferência() {
     }
 
     else {
-        
-        raio = raio
+
+        let valor_raio 
+        valor_raio = raio
     
         comprimento = Number(2 * Math.PI * raio).toFixed(2)
         area = Number(Math.PI * raio * raio).toFixed(2)
 
-        resultado.innerHTML = `Comprimento: <mark>${comprimento}</mark> <br> Área: <mark>${area}</mark>`
+        resultado.innerHTML = `Área: <mark>${area}</mark> <br> comprimento: <mark>${comprimento}</mark>`
     
     }
     
