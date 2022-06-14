deaed28a375e2a80d1228fc2e8eb984cb9514450
Lucas Renan <lucasrenandns96@gmail.com>
2022-06-14 10:34:24 -0300

    Update script.js

--- estudojs-002/maior-numero-informado/script.js
+++ estudojs-002/maior-numero-informado/script.js
@@ -34,7 +34,7 @@ function verificarMaior() {
 
         if (n1 == parseInt(n2) && n2 == parseInt(n3) && n3 == parseInt(n1) && n3 == parseInt(n2)) {
 
-            resultado.innerHTML = "todos os números são iguais"
+            resultado.innerHTML = "Todos os números são iguais"
         }
 
     else {
