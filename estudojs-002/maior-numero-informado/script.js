d658cf2a3d20f4f76ea6c56145df99207046771d
Lucas Renan <lucasrenandns96@gmail.com>
2022-06-13 21:15:05 -0300

    maior número

---
+++ estudojs-002/maior-numero-informado/script.js
@@ -0,0 +1,35 @@
+
+const button = document.querySelector("#button") 
+
+button.addEventListener('click', verificarMaior)
+
+function verificarMaior() {
+
+    const resultado = document.querySelector("#resultado")
+
+    const n1 = parseFloat(window.prompt("Informe um número: "))
+    const n2 = parseFloat(window.prompt("Informe outro: "))
+    const n3 = parseFloat(window.prompt("Informe mais um: "))
+
+    var maior
+
+    maior = n1
+   
+    if (n2 > n1 ) {
+
+        maior = n2
+    }
+    
+    if (n3 > maior) {
+
+        maior = n3
+    }
+
+    else {
+
+        resultado.innerHTML = "Todos os números são iguais!"
+    }
+
+    resultado.innerHTML = `O maior valor informado foi o ${maior}`
+
+}
