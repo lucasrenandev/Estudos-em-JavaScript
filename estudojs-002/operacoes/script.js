b0409af9c6a356d89d6009e0a9ea0227dc566243
Lucas Renan <lucasrenandns96@gmail.com>
2022-06-20 18:17:50 -0300

    Update script.js

--- estudojs-002/operacoes/script.js
+++ estudojs-002/operacoes/script.js
@@ -11,39 +11,48 @@ function verificadorNumeros() {
     const n2 = Number(window.prompt('Digite outro: '))
     const operacao = Number(window.prompt('Qual operação/verificação deseja realizar? \n[1] Par ou Ímpar \n[2] Positivo ou Negativo \n[3] Inteiro ou Decimal'))
 
-    let soma 
-    soma = n1 + n2
+    if (n1 !== Number(n1) || n2 !== Number(n2)) {
 
-    if (operacao == 1) {
-
-        if (soma % 2 == 0) {
-            analise = 'O número é <mark>Par</mark>'
-        }
-        else {
-            analise = 'O número é <mark>impar</mark>'
-        }
+        resultado.innerHTML = "[ERRO] Informe somente números!"
     }
-    else if (operacao == 2) {
 
-        if (soma > 0) {
-            analise = 'O valor é <mark>Positivo</mark>'
-        }
-        else {
-            analise = 'O valor é <mark>Negativo</mark>'
-        }
-    }
-    else if (operacao == 3) {
-
-        if (soma % 1 == 0) {
-            analise = 'O valor é <mark>Inteiro</mark>'
-        }
-        else {
-            analise = 'O valor é <mark>Decimal</mark>'
-        }
-    }
     else {
-        analise = 'Impossível realizar operação. Valores não informados!'
-    }
 
-    resultado.innerHTML = analise
+        let soma 
+        soma = n1 + n2
+    
+        if (operacao == 1) {
+    
+            if (soma % 2 == 0) {
+                analise = 'O número é <mark>Par</mark>'
+            }
+            else {
+                analise = 'O número é <mark>impar</mark>'
+            }
+        }
+        else if (operacao == 2) {
+    
+            if (soma > 0) {
+                analise = 'O valor é <mark>Positivo</mark>'
+            }
+            else {
+                analise = 'O valor é <mark>Negativo</mark>'
+            }
+        }
+        else if (operacao == 3) {
+    
+            if (soma % 1 == 0) {
+                analise = 'O valor é <mark>Inteiro</mark>'
+            }
+            else {
+                analise = 'O valor é <mark>Decimal</mark>'
+            }
+        }
+        else {
+            analise = 'Impossível realizar operação. Valores não informados!'
+        }
+    
+        resultado.innerHTML = analise
+    }
+   
 }   
